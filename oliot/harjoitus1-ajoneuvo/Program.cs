4aa4b63e1dad3e3871b300dc10cf77e2d459cfba
ZoharItzhak <zoharjitzhak@gmail.com>
2022-01-24 10:24:01 +0200

    ensimmainen commit

---
+++ oliot/harjoitus1-ajoneuvo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace harjoitus1_ajoneuvo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tahan tulee kaikki oliot kutsut
+
+            //anmetaan olion sisallo
+
+            //kutsutaan metodit
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
