2b7774e974cec18062329a32ebdfc59ee3f01a08
ZoharItzhak <zoharjitzhak@gmail.com>
2022-02-14 12:19:05 +0200

    commit all

--- oliot/harojoitus3-kiaas/Program.cs
+++ oliot/harojoitus3-kiaas/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace harojoitus3_kiuas
+namespace harjoitus3_kiuas
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            kiuas kiuas1 = new kiuas(0,0,false);
+
+            while (true)
+            {
+
+                kiuas1.tulasta();
+
+                string asetus = Console.ReadLine();
+
+                if (asetus.Equals("1"))
+
+                {
+                    kiuas1.paalle();
+                }
+                else if (asetus.Equals("2"))
+
+                {
+                    kiuas1.nostalampo();
+                }
+                else if (asetus.Equals("3"))
+
+                {
+                    kiuas1.laskelampo();
+                }
+                else if (asetus.Equals("4"))
+
+                {
+                    kiuas1.heitaloyly();
+                }  
+                else if (asetus.Equals("4"))
+
+                {
+                    break;
+                }
+            }
         }
     }
 }
---
+++ oliot/harojoitus3-kiaas/kiuas.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace harjoitus3_kiuas
+{
+     class kiuas
+    {
+        private int kosteus;
+
+        private int lampotila;
+
+        private Boolean tila;
+
+        public kiuas(int _kost, int _lamp, Boolean _tila)
+
+        {
+            kosteus = _kost;
+
+            lampotila = _lamp;
+
+            tila = _tila;
+        }
+
+
+        public void paalle()
+        {
+            if (tila == true)
+            {
+                tila = false;
+                Console.WriteLine("kiuas on nyt pois paalta");
+
+            }
+            if (tila == false)
+            {
+                tila = true;
+                Console.WriteLine("kiuas on nyt paalla");
+
+            }
+
+        }
+        public void nostalampo()
+        {
+            lampotila = +5;
+        }
+        public void laskelampo()
+        {
+            lampotila = -5;
+
+        }
+        public void heitaloyly()
+        {
+            kosteus = +5;
+
+        }
+        public void tulasta()
+        {
+            Console.WriteLine("kiukaan tiedot:");
+            if (tila == true)
+            {
+                Console.WriteLine("kiuas on paalla");
+            }
+            else if (tila == false)
+            {
+                Console.WriteLine("kiuas on pois paalta");
+            }
+            Console.WriteLine("kiukaan lampotila:" + lampotila + " astetta");
+            Console.WriteLine("kiukaan kosteus:" + kosteus + " prosenttia");
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
